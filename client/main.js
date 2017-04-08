cf812f9a6419aa0f79e11268ef96f09288c2c983
svetigrah <tomi@entropia.hr>
2017-04-08 13:38:02 +0000

    Dynamic template example

--- client/main.js
+++ client/main.js
@@ -6,16 +6,21 @@ import './main.html';
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
+  this.targetTemplate = new ReactiveVar('');
 });
 
 Template.hello.helpers({
   counter() {
     return Template.instance().counter.get();
   },
+  getTemplateName() {
+    return Template.instance().targetTemplate.get();
+  }
 });
 
 Template.hello.events({
   'click button'(event, instance) {
+    instance.targetTemplate.set('info');
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
   },
