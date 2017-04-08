210d4ecc3cc9208dbd0183114fac2bf2f79ea495
Ivan Sinek <isinek@gmail.com>
2017-04-08 13:49:40 +0200

    First templates

--- client/main.js
+++ client/main.js
@@ -3,20 +3,15 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.main.onCreated(function mainOnCreated() {
+  this.targetTemplate = new ReactiveVar('home');
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.main.helpers({
+  getTemplateName() {
+    return Template.instance().targetTemplate.get();
+  }
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.main.events({
 });
